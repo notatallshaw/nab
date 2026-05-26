707b4859068fb8b6d537c06c9f3e4ffdd7a192be
Damian Shaw <damian.peter.shaw@gmail.com>
2026-05-26 03:07:01 -0400

    Lowercase index hash digests on ingest

---
+++ nab-python/tests/test_simple_client_hashes.py
@@ -0,0 +1,25 @@
+"""Tests for hash-digest lowercasing in nab_index's Simple API JSON parser."""
+
+from __future__ import annotations
+
+from nab_index.client import _parse_files
+
+
+def _file(hashes: dict[str, str]) -> dict[str, object]:
+    return {
+        "filename": "foo-1.0-py3-none-any.whl",
+        "url": "https://example.com/foo/foo-1.0-py3-none-any.whl",
+        "hashes": hashes,
+    }
+
+
+def test_single_hash_digest_lowercased() -> None:
+    data = {"files": [_file({"sha256": "A" * 64})]}
+    files = _parse_files(data, "https://example.com/", "foo")
+    assert files[0].hashes == (("sha256", "a" * 64),)
+
+
+def test_multi_hash_digests_lowercased() -> None:
+    data = {"files": [_file({"sha256": "A" * 64, "sha512": "B" * 128})]}
+    files = _parse_files(data, "https://example.com/", "foo")
+    assert dict(files[0].hashes) == {"sha256": "a" * 64, "sha512": "b" * 128}
